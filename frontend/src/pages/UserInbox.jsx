4e421b2a6ed22d035e526a03b977bd0942d9b2a9
hachem rifaii <107303596+hachem-rifaii@users.noreply.github.com>
2024-11-29 16:13:25 -0800

    added socket io to project

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { TfiGallery } from "react-icons/tfi";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import styles from "../style/style";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://socket-server-eshop-kgyq.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
